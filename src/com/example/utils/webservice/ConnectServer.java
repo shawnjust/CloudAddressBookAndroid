87cb3f49d4b9832ff6939b8a90c8944b7bc9c028
horaceking <horaceking1992@gmail.com>
2014-10-20 22:49:36 +0800

    server

--- src/com/example/utils/webservice/ConnectServer.java
+++ src/com/example/utils/webservice/ConnectServer.java
@@ -52,11 +52,8 @@ public class ConnectServer {
 		return 6000;
 	}
 	
-	/**
-	 * 获得未读询盘总数
-	 * @return 未读询盘总数，网络连接失败返回null
-	 */
-	public int getUnreadedXunpanNum(){
+	
+	public int getMyFavNum(){
 		try{
 			Thread.sleep(3000);
 		}catch(Exception e){
@@ -65,11 +62,8 @@ public class ConnectServer {
 		return 312;
 	}
 	
-	/**
-	 * 获得访问总人数
-	 * @return 总人数，网络连接失败返回null
-	 */
-	public int getVisitNum(){
+	
+	public int getSubMeNum(){
 		try{
 			Thread.sleep(3000);
 		}catch(Exception e){
@@ -77,11 +71,17 @@ public class ConnectServer {
 		}
 		return 10000;
 	}
-	/**
-	 * 获得访问总次数
-	 * @return 访问总次数，网络连接失败返回null
-	 */
-	public int getVisitTime(){
+	
+	public int getTotalNum(){
+		try{
+			Thread.sleep(3000);
+		}catch(Exception e){
+			
+		}
+		return 10000;
+	}
+	//互相关注
+	public int getFriendsNum(){
 		try{
 			Thread.sleep(3000);
 		}catch(Exception e){
@@ -89,34 +89,7 @@ public class ConnectServer {
 		}
 		return 31200;
 	}
-	/**
-	 * 获得公告
-	 * @return 公告数组，网络连接失败返回null
-	 */
-	public ArrayList<String> getNoticeList(){
-		try{
-			Thread.sleep(3000);
-		}catch(Exception e){
-			
-		}
-		ArrayList<String> result = new ArrayList<String>();
-		result.add("重 要 通 知\n"
-				+ "远程市场开发，距离的增加，会促使采购商对您企业不信任程度的增加！\n"
-				+ "我们在回访中发现，使用国际在线视频营销的客户，其询盘转化率大大高于使用前，最高达一倍左右！\n"
-				+ "因此，为了提高ETW老客户的询盘收获率，和感谢老客户多年来对ETW国际的支持和认可，ETW国际决定：从2014年8月1日开始，为选择视频营销的老客户免除9000元的拍摄费用，截止时间为：2014年10月31日，特此敬告！\n"
-				+ "ETW国际客服部！");
-		result.add("重 要 通 知\n"
-				+ "远程市场开发，距离的增加，会促使采购商对您企业不信任程度的增加！\n"
-				+ "我们在回访中发现，使用国际在线视频营销的客户，其询盘转化率大大高于使用前，最高达一倍左右！\n"
-				+ "因此，为了提高ETW老客户的询盘收获率，和感谢老客户多年来对ETW国际的支持和认可，ETW国际决定：从2014年8月1日开始，为选择视频营销的老客户免除9000元的拍摄费用，截止时间为：2014年10月31日，特此敬告！\n"
-				+ "ETW国际客服部！");
-		result.add("重 要 通 知\n"
-				+ "远程市场开发，距离的增加，会促使采购商对您企业不信任程度的增加！\n"
-				+ "我们在回访中发现，使用国际在线视频营销的客户，其询盘转化率大大高于使用前，最高达一倍左右！\n"
-				+ "因此，为了提高ETW老客户的询盘收获率，和感谢老客户多年来对ETW国际的支持和认可，ETW国际决定：从2014年8月1日开始，为选择视频营销的老客户免除9000元的拍摄费用，截止时间为：2014年10月31日，特此敬告！\n"
-				+ "ETW国际客服部！");
-		return result;
-	}
+	
 	
 	/**
 	 * 分页获得通知内容
